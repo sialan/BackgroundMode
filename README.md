110193afe26dc3f97c395018505ea41445a3fb8d
Alan Si <si.alan090@gmail.com>
2013-05-23 17:50:20 -0700

    upgraded plugin to cordova 2.6

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ BackgroundMode
 ==============
 
 Protecting PhoneGap applications on iOS from getting suspended
+
+Add the following to config.xml
+<plugin name="BackgroundMode" value="BackgroundMode" onload="true" />
--- SampleApp.js
+++ SampleApp.js
@@ -1,25 +1,17 @@
-
-var Utils =
-{
-	startBackgroundMode: function()
-	{
+var Utils = {
+	startBackgroundMode: function() {
 		cordova.exec(null, null, "BackgroundMode", "start");
 	},
-
-	stopBackgroundMode: function()
-	{
+	stopBackgroundMode: function() {
 		cordova.exec(null, null, "BackgroundMode", "end");
 	}
 };
 
-function longRunningTask()
-{
+function longRunningTask() {
 	Utils.startBackgroundMode();
-
 	// Start a long-running process here.
 	// It will run even after the user has switched to another app but no longer than backgroundTimeRemaining (typically 10 minutes) - see
 	// http://developer.apple.com/library/ios/#documentation/UIKit/Reference/UIApplication_Class/Reference/Reference.html#//apple_ref/occ/instp/UIApplication/backgroundTimeRemaining
-
 	Utils.stopBackgroundMode();
 }
 
